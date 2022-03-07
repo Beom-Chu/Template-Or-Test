73c7c4196cda70e7f36aa41d71288fbf4fc241ae
kbs <chorocsky@gmail.com>
2022-03-07 13:11:32 +0900

    first

---
+++ src/main/java/com/kbs/templateortest/TemplateOrTestApplication.java
@@ -0,0 +1,13 @@
+package com.kbs.templateortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateOrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateOrTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbs/templateortest/TemplateOrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbs.templateortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateOrTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
