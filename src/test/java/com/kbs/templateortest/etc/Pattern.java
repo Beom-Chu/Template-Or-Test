c5b75492fe6a1a995d3229de7a03a19851a01e89
bumsoo <chorocsky@gmail.com>
2022-07-07 14:11:05 +0900

    Pattern test

---
+++ src/test/java/com/kbs/templateortest/etc/Pattern.java
@@ -0,0 +1,26 @@
+package com.kbs.templateortest.etc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
+
+import java.util.regex.Matcher;
+
+public class Pattern {
+    @Test
+    public void test() {
+
+
+        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("\\.[a-zA-Z0-9]+Api\\.");
+        String name = "kr.co.test.abc.def.member.info.noAuthApi.noApi.TestabcdApi";
+        Matcher matcher = pattern.matcher(name);
+
+        matcher.find(); // 복수 매치되더라도 첫번째 것만 체크한다.
+
+        System.out.println("[[[matcher = " + matcher.start());
+        System.out.println("[[[matcher.end() = " + matcher.end());
+
+        String partnerName = name.substring(matcher.start() + 1, matcher.end() - 4);
+
+        System.out.println("[[[partnerName = " + partnerName);
+    }
+}
