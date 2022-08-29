64b79d68a0e7fac04fa84750f4d47482aa6d1972
bumsoo <chorocsky@gmail.com>
2022-08-29 18:14:43 +0900

    Calendar test 추가

--- src/test/java/com/kbs/templateortest/etc/CalendarTest.java
+++ src/test/java/com/kbs/templateortest/etc/CalendarTest.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.TimeZone;
 
 public class CalendarTest {
@@ -40,5 +41,61 @@ public class CalendarTest {
 
         System.out.println("[[[ZonedDateTime.ofInstant(calendar.toInstant(), zoneId).with(LocalTime.MIN) = " + ZonedDateTime.ofInstant(calendar.toInstant(), zoneId).with(LocalTime.MIN));
         System.out.println("[[[ZonedDateTime.ofInstant(calendar.toInstant(), zoneId).with(LocalTime.MAX = " + ZonedDateTime.ofInstant(calendar.toInstant(), zoneId).with(LocalTime.MAX).withNano(0));
+    }
+
+    @Test
+    public void test2() {
+        String[] weekDay = { "일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일" };
+        Calendar cal = Calendar.getInstance();
+        int num = cal.get(Calendar.DAY_OF_WEEK)-1;
+
+        System.out.println("[[[weekDay[num] = " + weekDay[num]);
+
+
+        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy-MM-dd");
+
+        Calendar c = Calendar.getInstance();
+        c.add(c.DATE,-1);
+
+        String format = formatter.format(c.getTime());
+        System.out.println("[[[format = " + format);
+
+    }
+
+    @Test
+    public void test3() {
+
+        System.out.println("[[[format = " + getLastWeekMonday("yyyy-MM-dd"));
+
+        System.out.println("[[[format = " + getLastWeekSunday("yyyy-MM-dd"));
+    }
+
+    private static String getLastWeekMonday(String format){
+        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(format);
+
+        Calendar c = Calendar.getInstance();
+        c.add(c.DATE,-7);
+        c.set(Calendar.DAY_OF_WEEK,Calendar.MONDAY);
+
+        return formatter.format(c.getTime());
+    }
+
+    private static String getLastWeekSunday(String format){
+        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(format);
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_WEEK,Calendar.SUNDAY);
+
+        return formatter.format(c.getTime());
+    }
+
+    @Test
+    public void test4() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(calendar.DATE,8);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(calendar.toInstant(), TimeZone.getDefault().toZoneId()).with(LocalTime.MIN);
+
+        System.out.println("[[[zonedDateTime = " + zonedDateTime);
     }
 }
