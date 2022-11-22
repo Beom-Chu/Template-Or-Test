384d549ec1c73f29901d831db5aaec41b0af148b
bumsoo <chorocsky@gmail.com>
2022-11-22 09:37:09 +0900

    LocalDateTime test (joda.time <-> java.time)

--- src/test/java/com/kbs/templateortest/etc/LocalDateTimeTest.java
+++ src/test/java/com/kbs/templateortest/etc/LocalDateTimeTest.java
@@ -2,7 +2,16 @@ package com.kbs.templateortest.etc;
 
 import org.junit.jupiter.api.Test;
 
+/*
 
+WARN 11932 --- [  restartedMain] o.s.data.convert.CustomConversions       : Registering converter from class java.time.LocalDateTime to class org.joda.time.LocalDateTime as reading converter although it doesn't convert from a store-supported type; You might want to check your annotation setup at the converter implementation
+
+해당 경고 문구는 spring-boot-starter-data-mongodb 와 joda-time 라이브러리를 같이 사용하는 경우 발생 합니다.
+spring-boot-starter-data-mongodb 에 포함되어 있는 org.springframework.data:spring-data-commons:2.7.3 종속 요소에서
+변환 가능한 타입들의  converter를 설정하게 되는데, 해당 타입은 지원하지 않기 때문에 발생하는 것으로 보입니다.
+java.time.LocalDateTime -> org.joda.time.LocalDateTime
+
+ */
 public class LocalDateTimeTest {
 
     @Test
