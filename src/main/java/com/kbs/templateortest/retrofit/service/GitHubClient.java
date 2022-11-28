722156465afe8b9e8dc1c0418e0aca73bb690697
bumsoo <chorocsky@gmail.com>
2022-11-28 17:39:55 +0900

    Retrofit timeout 설정

--- src/main/java/com/kbs/templateortest/retrofit/service/GitHubClient.java
+++ src/main/java/com/kbs/templateortest/retrofit/service/GitHubClient.java
@@ -1,8 +1,11 @@
 package com.kbs.templateortest.retrofit.service;
 
+import okhttp3.OkHttpClient;
 import org.springframework.stereotype.Component;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class GitHubClient {
@@ -12,9 +15,18 @@ public class GitHubClient {
     private Retrofit retrofit = null;
 
     private Retrofit getClient() {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.MINUTES) // 연결 타임아웃(default : 10초)
+                .readTimeout(30, TimeUnit.SECONDS) // 읽기 타임아웃(default : 10초)
+                .writeTimeout(15, TimeUnit.SECONDS) // 쓰기 타임아웃(default : 10초)
+                .build();
+
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
