2975f2c0b47ee005f93850598f452d0f807afbbc
bumsoo <chorocsky@gmail.com>
2023-04-14 16:03:23 +0900

    동기 kafka send test

--- src/main/java/com/kbs/templateortest/kafka/sender/KafkaSender.java
+++ src/main/java/com/kbs/templateortest/kafka/sender/KafkaSender.java
@@ -2,12 +2,16 @@ package com.kbs.templateortest.kafka.sender;
 
 import com.kbs.templateortest.kafka.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutionException;
 
 @Component
 @RequiredArgsConstructor
@@ -39,5 +43,26 @@ public class KafkaSender {
     public void senderKey(UserDto dto) {
         String key = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S"));
         kafkaTemplate.send(TOPIC_NAME_USR, key, dto);
+    }
+
+    /**
+     * KafkaTemplate를 사용하여 send 메시지를 동기적으로 전송하는 방법.
+     * KafkaTemplate의 send 메서드에 get() 메서드를 호출.
+     * get() 메서드는 Future 인터페이스를 구현하는 ListenableFuture 타입의 객체를 반환하며,
+     * 이 객체는 Kafka Producer가 메시지를 전송한 결과를 반환.
+     * 따라서 get() 메서드를 호출하면 Kafka Producer가 메시지를 전송한 결과를 기다릴 수 있음.
+     */
+    public void syncSender(String str) throws ExecutionException, InterruptedException {
+        ListenableFuture<SendResult<Object, Object>> future = kafkaTemplate.send(TOPIC_NAME_STR, str);
+        SendResult<Object, Object> result = future.get();
+        RecordMetadata recordMetadata = result.getRecordMetadata();
+
+        System.out.println("[[[recordMetadata.topic = "         + recordMetadata.topic());
+        System.out.println("[[[recordMetadata.partition = "     + recordMetadata.partition());
+        System.out.println("[[[recordMetadata.offset = "        + recordMetadata.offset());
+        System.out.println("[[[recordMetadata.hasOffset = "     + recordMetadata.hasOffset());
+        System.out.println("[[[recordMetadata.timestamp = "     + recordMetadata.timestamp());
+        System.out.println("[[[recordMetadata.hasTimestamp = "  + recordMetadata.hasTimestamp());
+
     }
 }
--- src/test/java/com/kbs/templateortest/kafka/KafkaSenderTest.java
+++ src/test/java/com/kbs/templateortest/kafka/KafkaSenderTest.java
@@ -1,12 +1,16 @@
 package com.kbs.templateortest.kafka;
 
+import com.kbs.templateortest.kafka.config.KafkaConfig;
 import com.kbs.templateortest.kafka.dto.UserDto;
 import com.kbs.templateortest.kafka.sender.KafkaSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootTest
 public class KafkaSenderTest {
@@ -43,5 +47,11 @@ public class KafkaSenderTest {
 
         UserDto dto2 = new UserDto(1, "kbs", 36, LocalDateTime.now());
         kafkaSender.senderKey(dto2);
+    }
+
+    @Test
+    public void testSyncSend() throws ExecutionException, InterruptedException {
+        String str = "kafka!!" + Math.random();
+        kafkaSender.syncSender(str);
     }
 }
