05bc26a7807bdfd4e451c4e7e5c527489350798b
bumsoo <chorocsky@gmail.com>
2023-05-26 15:45:05 +0900

    HttpRequestMethodNotSupportedException Handle Test

--- src/main/java/com/kbs/templateortest/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kbs/templateortest/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kbs.templateortest.exception.handler;
 
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +10,13 @@ public class GlobalExceptionHandler {
     /* NullPointerException.class에 해당하는 오류 발생시 아래와 같이 처리 */
     @ExceptionHandler({NullPointerException.class})
     public String nullPointerExceptionHandle(NullPointerException ex){
-        return "nullPointerException Handle!!!";
+        return "nullPointerException 발생!!!";
     }
+
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public String httpRequestMethodNotSupportedExceptionHandle(HttpRequestMethodNotSupportedException ex){
+        return "HttpRequestMethodNotSupportedException 발생!!!";
+    }
+
+
 }
