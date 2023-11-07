049b8d6348710f031ce1a31054704c6b91c830b1
bumsoo <chorocsky@gmail.com>
2023-11-07 13:04:01 +0900

    ZonedDateTime test

--- src/test/java/com/kbs/templateortest/time/ZonedDateTimeTest.java
+++ src/test/java/com/kbs/templateortest/time/ZonedDateTimeTest.java
@@ -3,6 +3,8 @@ package com.kbs.templateortest.time;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 
 public class ZonedDateTimeTest {
@@ -14,5 +16,16 @@ public class ZonedDateTimeTest {
 
         System.out.println("[[[startOfMonth = " + startOfMonth);
 
+    }
+
+    @Test
+    void test2() {
+        ZonedDateTime firstOfWeek = ZonedDateTime.now().with(ChronoField.DAY_OF_WEEK, 1);
+        ZonedDateTime lastMonday = firstOfWeek.minusWeeks(1).truncatedTo(ChronoUnit.DAYS);
+        ZonedDateTime twoMondaysAgo = lastMonday.minusWeeks(1).truncatedTo(ChronoUnit.DAYS);
+
+        System.out.println("[[[firstOfWeek = " + firstOfWeek);
+        System.out.println("[[[lastMonday = " + lastMonday);
+        System.out.println("[[[twoMondaysAgo = " + twoMondaysAgo);
     }
 }
