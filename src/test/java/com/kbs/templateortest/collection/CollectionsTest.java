8fc647206694c8dab3d2bbdfe2e998338b70dad3
bskim <chorocsky@gmail.com>
2024-09-03 16:43:52 +0900

    Collections frequency, removeIf Test

--- src/test/java/com/kbs/templateortest/collection/CollectionsTest.java
+++ src/test/java/com/kbs/templateortest/collection/CollectionsTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class CollectionsTest {
+class CollectionsTest {
 
     @Test
     @DisplayName("Collections 메소드 shuffle 테스트")
@@ -25,6 +26,36 @@ public class CollectionsTest {
         Collections.shuffle(list, secureRandom);
         System.out.println("list = " + list);
         Collections.shuffle(list, secureRandom);
+        System.out.println("list = " + list);
+    }
+
+    @Test
+    @DisplayName("frequency 테스트")
+    void testFrequency() {
+
+        List<String> list = new ArrayList<>(Arrays.asList("a", "b", "b", "c", "d", "e", "e", "e", "f", "g"));
+
+        // 리스트 내에 b의 빈도수
+        int frequencyB = Collections.frequency(list, "b");
+        System.out.println("frequencyB = " + frequencyB);
+
+        // 리스트 내에 e의 빈도수
+        int frequencyE = Collections.frequency(list, "e");
+        System.out.println("frequencyE = " + frequencyE);
+    }
+
+    @Test
+    @DisplayName("removeIf 테스트")
+    void testRemoveIf() {
+
+        List<String> list = new ArrayList<>(Arrays.asList("a", "b", "b", "c", "d", "e", "e", "e", "f", "g"));
+        // a로 시작하는 문자 제거
+        list.removeIf(s -> s.startsWith("a"));
+        System.out.println("list = " + list);
+
+        // 리스트 내에 중복되는 문자 제거
+        // 리스트 내에 빈도수가 1 이상인 문자 제거
+        list.removeIf(s -> Collections.frequency(list, s) > 1);
         System.out.println("list = " + list);
     }
 }
